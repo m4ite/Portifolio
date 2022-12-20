5b800049258425ff74f035efdf024aa98c630245
m4ite <maitesenaibosch@gmail.com>
2022-12-19 21:36:23 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(function() {
+                
+    setTimeout(function() {
+        $('.fly-in-text') //a ação sera feita nos elementos da classe fly-in-text
+        .removeClass('hidden'); //remover a classe hidden que esta fazendo as letras ficarem invisiveis
+    }, 500); //vai demorar meio segundo
+    
+})();
